411c884644e78a26831dd2534d185ec2575408d1
RITA DE LA TORRE <65496428+ritadelatorre@users.noreply.github.com>
2022-03-15 09:35:18 +0100

    Add files via upload

---
+++ M2/test.js
@@ -0,0 +1,22 @@
+function calcular() {
+   document.getElementById('p1').innerHTML = '<b>Tu orden de prioridades:</b>';
+   document.getElementById('vaca').innerHTML = 'Tu Carrera';
+   document.getElementById('tigre').innerHTML = 'El Orgullo';
+   document.getElementById('oveja').innerHTML = 'El Amor';
+   document.getElementById('caballo').innerHTML = 'La familia';
+   document.getElementById('mono').innerHTML = 'El Dinero';
+
+   document.getElementById('p2').innerHTML = '<b>Como ves la vida:</b>';
+   document.getElementById('perro').innerHTML = 'Yo Soy...';
+   document.getElementById('gato').innerHTML = 'Mi Pareja, ';
+   document.getElementById('rata').innerHTML = 'Lo que más odio:';
+   document.getElementById('cafe').innerHTML = 'Para mi el sexo es';
+   document.getElementById('mar').innerHTML = 'Y la vida...';
+
+   document.getElementById('p3').innerHTML = '<b>Tu gente:</b>';
+   document.getElementById('amarillo').innerHTML = 'Nunca te olvidará';
+   document.getElementById('naranja').innerHTML = 'Puede ser tu gran amigo/a';
+   document.getElementById('blanco').innerHTML = 'Tu Alma Gemela:';
+   document.getElementById('rojo').innerHTML = 'Realmente amas a...';
+   document.getElementById('verde').innerHTML = 'Siempre estará en tu memoria';
+}
